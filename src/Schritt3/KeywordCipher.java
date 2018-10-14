5c65ec76716e7cc0cf4ff014ecc3e3241913e153
jborensky <jborensky@student.tgm.ac.at>
2018-10-14 10:51:32 +0200

    KeywordCipher implementiert. Volle Funktionsfähigkeit ausstehende - Bug-Fixing

--- src/Schritt3/KeywordCipher.java
+++ src/Schritt3/KeywordCipher.java
@@ -4,7 +4,9 @@ import Schritt1.MonoAlphabeticCipher;
 
 public class KeywordCipher extends MonoAlphabeticCipher{
 	private String keyword;
+	private String newAlph;
 	public KeywordCipher(String keyword) {
+		super();
 		this.setKeyword(keyword);
 		this.createAlphabet();
 	}
@@ -13,7 +15,69 @@ public class KeywordCipher extends MonoAlphabeticCipher{
 	}
 	public void createAlphabet() {
 		String Alphabet = "";
-		
+		String keyword = "";
+		this.keyword.toLowerCase();
+		while(true) {
+			if (checkForMoreThanOne(this.keyword)) break;
+			checkNumbOfLetters(this.keyword);
+		}
+		Alphabet = this.keyword + this.setnewAlph();
 		super.setSecretAlphabet(Alphabet);
+	}
+	
+	private String setnewAlph() {
+		char[] alph = this.getSecretAlphabet().toCharArray();
+		char[] word = this.keyword.toCharArray();
+		int w = 1;
+		for (int j = 0; j < word.length; j++) {
+			for (int i=w; i<alph.length; i++) {
+				if (word[j] == alph[i]) {
+					char[] hilfe = new char[alph.length-1];
+					for (int k = 0, l=0; k < hilfe.length; k++, l++) {
+						if (l!=i) {
+							hilfe[k] = alph[l];
+						} else {
+							k--;
+						}
+					}
+					alph = hilfe;
+				}
+			}
+			w++;
+		}
+		return String.valueOf(alph);
+	}
+	public void checkNumbOfLetters(String key) {
+		char[] keyword = key.toCharArray();
+		int w = 1;
+		for(int i = 0; i < keyword.length; i++){
+			for (int j = w; j<=keyword.length-1; j++) {
+				if((keyword[i]) == (keyword[j])){
+					char[] hilfe = new char[keyword.length-1];
+					for(int k = 0, l=0; k < keyword.length-1; k++, l++) {
+						if(l!=j-1) {
+							hilfe[k] = keyword[l];
+						} else {
+							k--;
+						}
+					}
+					keyword = hilfe;
+				}
+			}
+			w++;
+		}
+		this.keyword = String.valueOf(keyword);
+	}
+	
+	public boolean checkForMoreThanOne(String s1) {
+		boolean b1 = false;
+		for(int i = 0; i < s1.length(); i++){
+			if(s1.indexOf(s1.charAt(i)) == s1.lastIndexOf(s1.charAt(i))){
+				b1 = true;
+			} else {
+				return false;
+			}
+		}
+		return b1;
 	}
 }
---
+++ src/Schritt3/Test.java
@@ -0,0 +1,10 @@
+package Schritt3;
+
+public class Test {
+
+	public static void main(String[] args) {
+		KeywordCipher c1 = new KeywordCipher("Hallo");
+		System.out.println(c1.getSecretAlphabet());
+	}
+
+}
